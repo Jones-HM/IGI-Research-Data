fd430aafdd20b76d443abfe5b385c40534bbcc5e
IGI-Research-Devs <93740747+IGI-Research-Devs@users.noreply.github.com>
2022-04-23 17:13:52 +0400

    Add files via upload

---
+++ Research/MISC/WEAPONS_AMMO.cs
@@ -0,0 +1,17 @@
+Dictionary<string, string> ammoList = new Dictionary<string, string>()
+{
+        {"SPAS12/JACKHAMMER","AMMO_ID_12"},
+        {"COLT","AMMO_ID_357"},//AMMO_ID_44 Original slot fix.
+        {"M2HB","AMMO_ID_127"},
+        {"DESERTEAGLE","AMMO_ID_357"},
+        {"M16A2/MINIMI/T80","AMMO_ID_556"},
+        {"AK47","AMMO_ID_762"},
+        {"GLOCK/UZI/UZIX2/MP5SD","AMMO_ID_919"},
+        {"RPG18","AMMO_ID_1000"},
+        {"DRAGUNOV","AMMO_ID_DRAGUNOV"},
+        {"FLASHBANG","AMMO_ID_FLASHBANG"},
+        {"GRENADE","AMMO_ID_GRENADE"},
+        {"M203","AMMO_ID_M203"},
+        {"MEDIPACK","AMMO_ID_MEDIPACK"},
+        {"PROXIMITYMINE","AMMO_ID_PROXIMITYMINE"}
+};
